98f14f5fadf290e0c47f79fce46497d79f7e108f
larrybr <larrybr@noemail.net>
2022-12-02 03:35:28 +0000

    Experimental branch for more flexible selection of extensions and localization of details of incorporation by host. A WIP for now.
    FossilOrigin-Name: 98be19ba839779e6d5a22850fe82fb3d5d7ed156baf8074a4d4b74f4e49f3f61

---
+++ src/cli_extend.c
@@ -0,0 +1,24 @@
+/*
+** 2022-12-01
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+*************************************************************************
+**
+** This source allows multiple SQLite extensions to be either: (a) combined
+** into a single runtime-loadable library; (b) built into the SQLite shell
+** using a preprocessing convention set by src/shell.c.in (and shell.c), or
+** built into and statically linked with a customized SQLite3 library build.
+**
+** The customized library build may be effected by appending this source
+** to the sqlite3.c amalgamation to form a modified translation unit. How
+** this concatenation is accomplished is immaterial.
+*/
+
+INCLUDE cli_extend.c
+#include "cli_extend.c"
