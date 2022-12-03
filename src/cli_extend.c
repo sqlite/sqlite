4961fd900a07839c9ff7a82881ac0417c780ded6
larrybr <larrybr@noemail.net>
2022-12-03 17:19:11 +0000

    Make conglomerateur help with extension packaging. Otherwise a WIP check-in.
    FossilOrigin-Name: c4214d8d3ccc00669d69e7973213fabf111a0e95598911d8cc896a384fc18d66

--- src/cli_extend.c
+++ src/cli_extend.c
@@ -13,12 +13,16 @@
 ** This source allows multiple SQLite extensions to be either: (a) combined
 ** into a single runtime-loadable library; (b) built into the SQLite shell
 ** using a preprocessing convention set by src/shell.c.in (and shell.c), or
-** built into and statically linked with a customized SQLite3 library build.
+** (b) statically linked with a customized SQLite3 library build.
 **
 ** The customized library build may be effected by appending this source
 ** to the sqlite3.c amalgamation to form a modified translation unit. How
 ** this concatenation is accomplished is immaterial.
 */
 
-INCLUDE cli_extend.c
+#ifndef INCLUDE
+# define INCLUDE(fn)
+#endif
+
+INCLUDE(cli_extend.c)
 #include "cli_extend.c"
