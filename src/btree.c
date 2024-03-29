cd57479c6b537e0831731b11be278911335a880e
dan <Dan Kennedy>
2024-03-29 17:32:00 +0000

    Improve the log message emitted when a BEGIN CONCURRENT transaction cannot be committed due to conflicts so that it identifies the conflicting table in a few more cases.
    FossilOrigin-Name: 5d30e362cf72da3e17663dcb4299047ebe797ab6054fb14b2150ba82c2e698e1

--- src/btree.c
+++ src/btree.c
@@ -5683,6 +5683,9 @@ static int accessPayload(
               (eOp==0 ? PAGER_GET_READONLY : 0)
           );
           if( rc==SQLITE_OK ){
+            setMempageRoot(
+                (MemPage*)sqlite3PagerGetExtra(pDbPage), pCur->pgnoRoot
+            );
             aPayload = sqlite3PagerGetData(pDbPage);
             nextPage = get4byte(aPayload);
             rc = copyPayload(&aPayload[offset+4], pBuf, a, eOp, pDbPage);
@@ -9679,6 +9682,7 @@ static SQLITE_NOINLINE int btreeOverwriteOverflowCell(
   do{
     rc = btreeGetPage(pBt, ovflPgno, &pPage, 0);
     if( rc ) return rc;
+    setMempageRoot(pPage, pCur->pgnoRoot);
     if( sqlite3PagerPageRefcount(pPage->pDbPage)!=1 || pPage->isInit ){
       rc = SQLITE_CORRUPT_PAGE(pPage);
     }else{
