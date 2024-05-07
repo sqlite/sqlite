a40cae78027eed3212b0207c478a5879ba49bea2
dan <Dan Kennedy>
2024-05-07 17:58:07 +0000

    Ensure successive scans of pragma virtual tables return consistent rowids.
    FossilOrigin-Name: 6838bf3ac130c5862f5fbc718b3e49ece154fa840b6539af2616cffa31e73808

--- src/pragma.c
+++ src/pragma.c
@@ -2911,6 +2911,7 @@ static void pragmaVtabCursorClear(PragmaVtabCursor *pCsr){
   int i;
   sqlite3_finalize(pCsr->pPragma);
   pCsr->pPragma = 0;
+  pCsr->iRowid = 0;
   for(i=0; i<ArraySize(pCsr->azArg); i++){
     sqlite3_free(pCsr->azArg[i]);
     pCsr->azArg[i] = 0;
