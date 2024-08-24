58b08d77029b63a80cc7bb920819b177c06bdfe6
dan <Dan Kennedy>
2024-08-24 15:54:15 +0000

    Avoid a stack overflow that could be caused by a recursively defined WINDOW() with a strategically embedded error.
    FossilOrigin-Name: bada54bd6bf54190e40aa721b77081015957d204c7b6a9fdbe8c67bcf20798f8

--- src/resolve.c
+++ src/resolve.c
@@ -1300,7 +1300,7 @@ static int resolveExprStep(Walker *pWalker, Expr *pExpr){
           sqlite3WalkExprList(pWalker, pExpr->pLeft->x.pList);
         }
 #ifndef SQLITE_OMIT_WINDOWFUNC
-        if( pWin ){
+        if( pWin && pParse->nErr==0 ){
           Select *pSel = pNC->pWinSelect;
           assert( pWin==0 || (ExprUseYWin(pExpr) && pWin==pExpr->y.pWin) );
           if( IN_RENAME_OBJECT==0 ){
