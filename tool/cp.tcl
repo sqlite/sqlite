ae5ef1e80fc0597a211cd14c2cc3ab715301111a
drh <>
2024-10-24 15:57:21 +0000

    Fix the new tool/cp.tcl so that it works with older TCL versions, such as jimtcl.
    FossilOrigin-Name: 61f18c96183867fe9d0fb30b8b71c0253f40503e32c8a4202196fb6418f2f46e

--- tool/cp.tcl
+++ tool/cp.tcl
@@ -6,4 +6,23 @@
 #
 #   tclsh cp.tcl FILE1 FILE2 ... FILEN DIR
 #
-file copy -force -- {*}$argv
+
+# This should be as simple as
+#
+#     file copy -force -- {*}$argv
+#
+# But jimtcl doesn't support that.  So we have to do it the hard way.
+
+if {[llength $argv]<2} {
+  error "Usage: $argv0 SRC... DESTDIR"
+}
+set n [llength $argv]
+set destdir [lindex $argv [expr {$n-1}]]
+if {![file isdir $destdir]} {
+  error "$argv0: not a directory: \"$destdir\""
+}
+for {set i 0} {$i<$n-1} {incr i} {
+  set fn [file normalize [lindex $argv $i]]
+  set tail [file tail $fn]
+  file copy -force $fn [file normalize $destdir/$tail]
+}
