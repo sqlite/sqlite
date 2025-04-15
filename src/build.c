0243ca82459db9984f3708eb844b32bcebbff2e5
drh <>
2025-04-15 21:59:38 +0000

    Correctly handle the case of a multi-column UNIQUE constraint that contains the ROWID as one of it columns, and then the columns of that UNIQUE are used in a row-value IN operator as a WHERE clause constraint.  Reported by [forum:/forumpost/b9647a113b465950|forum post b9647a113b].  Problem introduced by [723f1be3d4a905a6], part of ticket [da78413751863].
    FossilOrigin-Name: d22475b81c4e26ccc50f3b5626d43b32f7a2de34e5a764539554665bdda735d5

--- src/build.c
+++ src/build.c
@@ -4219,6 +4219,7 @@ void sqlite3CreateIndex(
       assert( j<=0x7fff );
       if( j<0 ){
         j = pTab->iPKey;
+        pIndex->bIdxRowid = 1;
       }else{
         if( pTab->aCol[j].notNull==0 ){
           pIndex->uniqNotNull = 0;
