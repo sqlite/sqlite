53a6b7d13899d14ab87810be729b2ee94374159f
drh <>
2025-10-14 16:57:50 +0000

    Make the new sqlite3_db_status64() API accessible to extensions.
    FossilOrigin-Name: 2ebd7330312f3ee4674c343b7a19b703f1f89611a6846a0e2be39867f9895522

--- src/sqlite3ext.h
+++ src/sqlite3ext.h
@@ -370,6 +370,8 @@ struct sqlite3_api_routines {
   int (*setlk_timeout)(sqlite3*,int,int);
   /* Version 3.51.0 and later */
   int (*set_errmsg)(sqlite3*,int,const char*);
+  int (*db_status64)(sqlite3*,int,sqlite3_int64*,sqlite3_int64*,int);
+  
 };
 
 /*
@@ -707,6 +709,7 @@ typedef int (*sqlite3_loadext_entry)(
 #define sqlite3_setlk_timeout          sqlite3_api->setlk_timeout
 /* Version 3.51.0 and later */
 #define sqlite3_set_errmsg             sqlite3_api->set_errmsg
+#define sqlite3_db_status64            sqlite3_api->db_status64
 #endif /* !defined(SQLITE_CORE) && !defined(SQLITE_OMIT_LOAD_EXTENSION) */
 
 #if !defined(SQLITE_CORE) && !defined(SQLITE_OMIT_LOAD_EXTENSION)
