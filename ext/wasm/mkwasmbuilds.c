be4d0c2e2f65c76d39cfac8ca103dbbf1c6c2064
stephan <stephan@noemail.net>
2025-10-24 20:29:12 +0000

    Make it particularly clear in the wasm build output that unsupported builds are exactly that. Cosmetic tweak only, no functional changes.
    FossilOrigin-Name: 86f53a2eb37d8ec98bc9d4c6b00253cc00d798e79c7d409e65d608354a556e19

--- ext/wasm/mkwasmbuilds.c
+++ ext/wasm/mkwasmbuilds.c
@@ -444,6 +444,11 @@ static void mk_prologue(void){
     ps("endif");
   }
 
+  ps("define label.unsupported-build\n"
+     "$(emo.fire)$(emo.fire)$(emo.fire)Unsupported build:"
+     " use at your own risk!\n"
+     "endef");
+
   ps(zBanner
      /** $1 = build name */
      "b.call.wasm-strip = "
@@ -804,8 +809,8 @@ static void mk_lib_mode(const char *zBuildName, const BuildDef * pB){
     emit_compile_start(zBuildName);
 
     if( F_UNSUPPORTED & pB->flags ){
-      pf("\t@echo '$(logtag.%s) $(emo.fire)$(emo.fire)$(emo.fire): "
-         "unsupported build. Use at your own risk.'\n", zBuildName);
+      pf("\t@echo '$(logtag.%s) $(label.unsupported-build)'\n",
+         zBuildName);
     }
 
     /* emcc ... */
@@ -901,7 +906,11 @@ static void mk_lib_mode(const char *zBuildName, const BuildDef * pB){
 
     }
   }
-  pf("\t@$(call b.echo,%s,$(emo.done) done!)\n", zBuildName);
+  pf("\t@$(call b.echo,%s,$(emo.done) done!%s)\n",
+     zBuildName,
+     (F_UNSUPPORTED & pB->flags)
+     ? " $(label.unsupported-build)"
+     : "");
 
   pf("\n%dbit: $(out.%s.js)\n"
      "$(out.%s.wasm): $(out.%s.js)\n"
