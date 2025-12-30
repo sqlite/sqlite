01f2907d48c34aeca55a55cc7937f07d2542514b
dan <Dan Kennedy>
2025-12-30 11:33:30 +0000

    Fix the change on this branch so that it works with amalgamation builds.
    FossilOrigin-Name: 7f0b9e7a8bba1b73ef16257e72a055ea84fb10eb5cf0e6431fca81b2f040c5ee

--- ext/fts5/fts5Int.h
+++ ext/fts5/fts5Int.h
@@ -60,9 +60,6 @@ typedef sqlite3_uint64 u64;
 # define LARGEST_INT64  (0xffffffff|(((i64)0x7fffffff)<<32))
 # define SMALLEST_INT64 (((i64)-1) - LARGEST_INT64)
 
-# define LARGEST_INT32  ((int)(0x7fffffff))
-# define SMALLEST_INT32 ((int)((-1) - LARGEST_INT32))
-
 /*
 ** This macro is used in a single assert() within fts5 to check that an
 ** allocation is aligned to an 8-byte boundary. But it is a complicated
@@ -84,7 +81,13 @@ typedef sqlite3_uint64 u64;
 # define FLEXARRAY 1
 #endif
 
-#endif
+#endif /* SQLITE_AMALGAMATION */
+
+/*
+** Constants for the largest and smallest possible 32-bit signed integers.
+*/
+# define LARGEST_INT32  ((int)(0x7fffffff))
+# define SMALLEST_INT32 ((int)((-1) - LARGEST_INT32))
 
 /* Truncate very long tokens to this many bytes. Hard limit is 
 ** (65536-1-1-4-9)==65521 bytes. The limiting factor is the 16-bit offset
