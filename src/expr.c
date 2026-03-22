80b88b2482df643fcd0a27e95a1f8bc22c697e13
drh <>
2026-03-22 20:52:23 +0000

    Proposed new API routine: sqlite3_atof().
    FossilOrigin-Name: db370f0f628895279a1c1db79ba7428cb4e31b56cafc191859c2f1a5a924435b

--- src/expr.c
+++ src/expr.c
@@ -4302,8 +4302,7 @@ sqlite3ExprCodeIN_oom_error:
 */
 static void codeReal(Vdbe *v, const char *z, int negateFlag, int iMem){
   if( ALWAYS(z!=0) ){
-    double value;
-    sqlite3AtoF(z, &value);
+    double value = sqlite3_atof(z);
     assert( !sqlite3IsNaN(value) ); /* The new AtoF never returns NaN */
     if( negateFlag ) value = -value;
     sqlite3VdbeAddOp4Dup8(v, OP_Real, 0, iMem, 0, (u8*)&value, P4_REAL);
