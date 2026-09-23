d884a3c7c94dd4a8635f2f98097dd985f0f49408
drh <>
2026-09-23 11:37:04 +0000

    An attempt to improve the handling of rowid lookups using very large floating point values.
    FossilOrigin-Name: 881da860d6048106a50cfec463bd64ee2773baa996129dab369939252f8fcf48

--- src/vdbe.c
+++ src/vdbe.c
@@ -5661,7 +5661,7 @@ case OP_SeekRowid: {        /* jump0, in3, ncycle */
     }else
     if( (x.flags & MEM_Real)==0
      || x.u.r < -9223372036854775808.0
-     || x.u.r > 9223372036854775807.0
+     || x.u.r > 9223372036854774784.0
      || (double)(iKey = sqlite3RealToI64(x.u.r))!=x.u.r
     ){
       goto jump_to_p2;
